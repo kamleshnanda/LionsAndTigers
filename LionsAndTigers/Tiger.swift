2287a9dddafc8d91a8331ccfb847241a5d579f75
Metamorph2 <nandakamlesh@gmail.com>
2014-10-08 22:36:13 -0700

    Initial checkin

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Kamlesh Nanda on 10/8/14.
+//  Copyright (c) 2014 Kamlesh Nanda. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+}
